37274c68042808fe044e956393b8be964d53a68a
ViniciusDevelopment <viniciusmacielpires@gmail.com>
2024-06-22 17:46:36 -0300

    modais da movimentação

--- frontend/src/pages/DetalharMovimentacoes/index.js
+++ frontend/src/pages/DetalharMovimentacoes/index.js
@@ -1,18 +1,21 @@
 import React, { useEffect, useState } from 'react';
-import { View, Text, StyleSheet, FlatList, ActivityIndicator, TouchableOpacity } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity, Modal, TextInput, Button } from 'react-native';
 import axios from 'axios';
-import { useNavigation } from '@react-navigation/native';
 
 export default function DetalharMovimentacoes({ route, navigation }) {
   const { token, setValidationResult } = route.params;
   const [validationResultLocal, setValidationResultLocal] = useState(null);
   const { movimentacao } = route.params;
   const [categorias, setCategorias] = useState([]);
+  const [modalVisibleDelete, setModalVisibleDelete] = useState(false);
+  const [modalVisibleEdit, setModalVisibleEdit] = useState(false);
+  const [novoNomeMovimentacao, setNovoNomeMovimentacao] = useState(movimentacao.descricao);
+  const [novoValorMovimentacao, setNovoValorMovimentacao] = useState(movimentacao.valor);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     const validateToken = async () => {
       try {
-        console.log('token', token);
         const response = await axios.post('http://172.16.4.17:8000/api/Autenticacao/tokenvalidation', { token });
         setValidationResultLocal(response.data);
       } catch (error) {
@@ -23,25 +26,25 @@ export default function DetalharMovimentacoes({ route, navigation }) {
     };
 
     validateToken();
-  }, [token, setValidationResultLocal]);
+  }, [token]);
 
   useEffect(() => {
     if (validationResultLocal && validationResultLocal.id) {
-  const fetchCategorias = async () => {
-    try {
-      const responseCategorias = await axios.get('http://172.16.4.17:8000/api/Financa/categoria/', {
-        headers: {
-          Authorization: `Token ${token}`
+      const fetchCategorias = async () => {
+        try {
+          const responseCategorias = await axios.get('http://172.16.4.17:8000/api/Financa/categoria/', {
+            headers: {
+              Authorization: `Token ${token}`
+            }
+          });
+          setCategorias(responseCategorias.data);
+        } catch (error) {
+          console.error('Error fetching categorias:', error);
+          setError('Error fetching categorias');
         }
-      });
-      setCategorias(responseCategorias.data);
-    } catch (error) {
-      console.error('Error fetching categorias:', error);
-      setError('Error fetching categorias');
+      };
+      fetchCategorias();
     }
-  };
-  fetchCategorias();
-}
   }, [validationResultLocal, token]);
 
   const getCorPorTipo = (tipo) => {
@@ -57,7 +60,7 @@ export default function DetalharMovimentacoes({ route, navigation }) {
     }
   };
 
-const renderCategoria = (categoriaId) => {
+  const renderCategoria = (categoriaId) => {
     const categoria = categorias.find(cat => cat.id === categoriaId);
     if (categoria) {
       const cor = getCorPorTipo(categoria.tipo);
@@ -67,7 +70,38 @@ const renderCategoria = (categoriaId) => {
     }
     return null;
   };
-  
+
+  const handleDeleteMovimentacao = async () => {
+    try {
+      await axios.delete(`http://172.16.4.17:8000/api/Financa/movimentacao/${movimentacao.id}/`, {
+        headers: {
+          Authorization: `Token ${token}`
+        }
+      });
+      navigation.goBack(); // Voltar para a tela anterior após a exclusão
+    } catch (error) {
+      console.error('Error deleting movimentacao:', error);
+      setError('Error deleting movimentacao');
+    }
+  };
+
+  const handleEditMovimentacao = async () => {
+    try {
+      await axios.put(`http://172.16.4.17:8000/api/Financa/movimentacao/${movimentacao.id}/`, {
+        descricao: novoNomeMovimentacao,
+        valor: novoValorMovimentacao,
+        // Adicione outros campos que precisam ser atualizados
+      }, {
+        headers: {
+          Authorization: `Token ${token}`
+        }
+      });
+      navigation.goBack(); // Voltar para a tela anterior após a alteração
+    } catch (error) {
+      console.error('Error editing movimentacao:', error);
+      setError('Error editing movimentacao');
+    }
+  };
 
   return (
     <View style={styles.container}>
@@ -86,13 +120,76 @@ const renderCategoria = (categoriaId) => {
       
       <Text style={styles.label}>Data da Movimentação:</Text>
       <Text style={styles.value}>{movimentacao.movimentado_em}</Text>
+
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity style={[styles.button, styles.deleteButton]} onPress={() => setModalVisibleDelete(true)}>
+          <Text style={styles.buttonText}>Deletar</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={[styles.button, styles.editButton]} onPress={() => setModalVisibleEdit(true)}>
+          <Text style={styles.buttonText}>Alterar</Text>
+        </TouchableOpacity>
+      </View>
+
+      {/* Modal de Deleção */}
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={modalVisibleDelete}
+        onRequestClose={() => setModalVisibleDelete(false)}
+      >
+        <View style={styles.modalContainer}>
+          <View style={styles.modalContent}>
+            <Text>Deletar Movimentação</Text>
+            <Text>Tem certeza que deseja deletar esta movimentação?</Text>
+            <View style={styles.modalButtons}>
+              <TouchableOpacity onPress={() => setModalVisibleDelete(false)} style={[styles.button, { backgroundColor: 'red' }]}>
+                <Text style={styles.buttonText}>Cancelar</Text>
+              </TouchableOpacity>
+              <TouchableOpacity onPress={handleDeleteMovimentacao} style={[styles.button, { backgroundColor: 'green' }]}>
+                <Text style={styles.buttonText}>Deletar</Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+        </View>
+      </Modal>
+
+      {/* Modal de Edição */}
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={modalVisibleEdit}
+        onRequestClose={() => setModalVisibleEdit(false)}
+      >
+        <View style={styles.modalContainer}>
+          <View style={styles.modalContent}>
+            <Text>Alterar Movimentação</Text>
+            <TextInput
+              style={styles.input}
+              value={novoNomeMovimentacao}
+              onChangeText={setNovoNomeMovimentacao}
+              placeholder="Descrição da movimentação"
+            />
+            <TextInput
+              style={styles.input}
+              value={novoValorMovimentacao}
+              onChangeText={setNovoValorMovimentacao}
+              placeholder="Valor da movimentação"
+              keyboardType="numeric"
+            />
+            <View style={styles.modalButtons}>
+              <TouchableOpacity onPress={() => setModalVisibleEdit(false)} style={[styles.button, { backgroundColor: 'red' }]}>
+                <Text style={styles.buttonText}>Cancelar</Text>
+              </TouchableOpacity>
+              <TouchableOpacity onPress={handleEditMovimentacao} style={[styles.button, { backgroundColor: 'green' }]}>
+                <Text style={styles.buttonText}>Salvar</Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+        </View>
+      </Modal>
     </View>
   );
-
-
 }
-
-
 
 const styles = StyleSheet.create({
   container: {
@@ -114,5 +211,51 @@ const styles = StyleSheet.create({
     fontSize: 18,
     color: '#333',
     marginBottom: 10,
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    marginTop: 20,
+  },
+  button: {
+    padding: 10,
+    borderRadius: 5,
+  },
+  deleteButton: {
+    backgroundColor: 'red',
+  },
+  editButton: {
+    backgroundColor: 'green',
+  },
+  buttonText: {
+    color: '#fff',
+    fontWeight: 'bold',
+  },
+  modalContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'rgba(0,0,0,0.5)',
+  },
+  modalContent: {
+    width: 300,
+    padding: 20,
+    backgroundColor: '#fff',
+    borderRadius: 10,
+    alignItems: 'center',
+  },
+  modalButtons: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    marginTop: 20,
+    width: '100%',
+  },
+  input: {
+    width: '100%',
+    padding: 10,
+    borderColor: '#ccc',
+    borderWidth: 1,
+    borderRadius: 5,
+    marginTop: 10,
   },
 });
